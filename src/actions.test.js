3d9bf9f4ba489e4d7a391bdd7bc6129058b3543d
Ibrahim Qureshi <you@example.com>
2023-01-09 17:53:24 -0800

    added actions tests

--- src/actions.test.js
+++ src/actions.test.js
@@ -1,26 +1,61 @@
-import * as actions from './actions'
-import * as types from './constants'
-import configureMockStore from 'redux-mock-store'
-import thunkMiddleware from 'redux-thunk'
+import * as actions from './actions';
+import {
+  CHANGE_SEARCHFIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED
+ } from './constants';
+import configureMockStore from 'redux-mock-store';
+import thunkMiddleware from 'redux-thunk';
+import nock from "nock";
 
 export const mockStore = configureMockStore([thunkMiddleware]);
 
-describe('actions', () => {
-  it('should create an action to search', () => {
-    const text = 'Finish docs'
+describe('setSearchField', () => {
+  it('should create an action to search robots', () => {
+    const text = 'some search';
     const expectedAction = {
-      type: types.CHANGE_SEARCHFIELD,
+      type: CHANGE_SEARCHFIELD,
       payload: text
-    }
+    };
     expect(actions.setSearchField(text)).toEqual(expectedAction)
   })
 })
 
-describe("Fetch robots action PENDING", () => {
-  it("should creat a Pending action on request Robots", () => {
+describe("requestRobots", () => {
+  it("should create REQUEST_ROBOTS_PENDING", () => {
     const store = mockStore();
-    store.dispatch(actions.requestRobots())
+    store.dispatch(actions.requestRobots());
     const action = store.getActions();
-    expect(action[0]).toEqual({type: "REQUEST_ROBOTS_PENDING"});
+
+    const expectedAction = { type: REQUEST_ROBOTS_PENDING };
+
+    expect(action[0]).toEqual(expectedAction);
+  });
+
+  it ("should create REQUEST_ROBOTS_SUCCESS", () => {
+    const store = mockStore();
+    const data = { id: 5, name: "Jack", email: "jack@gmail.com" };
+
+    nock(actions.basePath).get(actions.endpoint).reply(200, data);
+    store.dispatch(actions.requestRobots());
+    const action = store.getActions();
+
+    const expectedAction = { type: REQUEST_ROBOTS_SUCCESS, payload: data };
+
+    expect(action).toEqual(expectedAction);
+  });
+
+  it ("should create REQUEST_ROBOTS_FAILED", () => {
+    const store = mockStore();
+    const error = "some error";
+
+    nock(actions.basePath).get(actions.endpoint).replyWithError(error);
+    store.dispatch(actions.requestRobots());
+    const action = store.getActions();
+
+    const expectedAction = { type: REQUEST_ROBOTS_FAILED, payload: error };
+
+    expect(action).toEqual(expectedAction);
   });
 });
